fbb92cc41a1be537db387d999a9d5381f5493f4e
JesusSotoT <soto.tovar.jesus@gmail.com>
2018-09-19 15:44:36 -0500

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'platzisquare';
+  title = 'PlatziSquare';
+lugares:any = [
+		{plan: 'Pagado',cercania: 1, distancia: 1 , active: true,nombre:'Floreria la Gardenia'},
+		{plan: 'Pagado',cercania: 1, distancia: 1.8 , active: false,nombre:'Donas la pasadita'},
+		{plan: 'gratuito',cercania: 2, distancia: 5 , active: true,nombre:'Veterinaria Huellitas felices'},
+		{plan: 'gratuito',cercania: 3, distancia: 10 , active: false,nombre:'Floreria la Gardenia'},
+		{plan: 'Pagado',cercania: 3, distancia: 35 , active: true,nombre:'Donas la pasadita'},
+		{plan: 'gratuito',cercania: 3, distancia: 120 , active: false,nombre:'Cementerio Aqui descansa tu mama'}
+
+];
+
+  lat: number = 20.712651;
+  lng: number = -103.409950;
+
+  constructor(){
+
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule, AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBe5iLsKytP4-SpeHfd0UUHza3ycaF5p0s'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
